5877360c94c5688b8593150d804e06fca07e50cc
Dmytro Ruslanovich <108448991+Blessed199@users.noreply.github.com>
2023-06-18 18:01:13 +0200

    ДЗ #3

---
+++ JS lesson/js/script.js
@@ -0,0 +1,22 @@
+
+
+
+// let userName = prompt('Як вас звати?', 'Гість')
+// document.write(`Привіт ${userName} на сайті`)
+ 
+// ---------------------------------------
+// let a = parseInt(prompt('First number', '0'))
+// let b = parseInt(prompt('Second number', '0'))
+
+// let s = a + b
+
+// document.write(`<h1> sum= ${s} </h1>`)
+// ----------------------------------------
+// Підрахувати загальну вагу  ящиків картоплі
+// let boxCount = parseInt(prompt('Кількість ящиків'))
+// let boxWeight = parseFloat(prompt('Вага одного ящика'))
+
+// let sum = boxCount * boxWeight
+
+// document.write(`Total weight = ${sum.toFixed(2)}`)
+
